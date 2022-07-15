7e8c10b1cfa54400e4685dcd84f9e032a49b20be
vivianbreedlove <vivianbreedlove@github.com>
2022-07-14 20:07:38 -0500

    random health and attack values

--- assets/js/game.js
+++ assets/js/game.js
@@ -33,8 +33,10 @@ var fight = function(enemyName) {
       }
     }
 
-    // remove enemy's health by subtracting the amount set in the playerAttack variable
-    enemyHealth = enemyHealth - playerAttack;
+    // generate random damage value based on player's attack power
+  var damage = randomNumber(playerAttack - 3, playerAttack);
+
+  enemyHealth = Math.max(0, enemyHealth - damage);
     console.log(
       playerName + ' attacked ' + enemyName + '. ' + enemyName + ' now has ' + enemyHealth + ' health remaining.'
     );
@@ -44,7 +46,7 @@ var fight = function(enemyName) {
       window.alert(enemyName + ' has died!');
 
       // award player money for winning
-      playerMoney = playerMoney + 20;
+      playerMoney = Math.max( 0, playerMoney + 20);
 
       // leave while() loop since enemy is dead
       break;
@@ -53,7 +55,10 @@ var fight = function(enemyName) {
     }
 
     // remove players's health by subtracting the amount set in the enemyAttack variable
-    playerHealth = playerHealth - enemyAttack;
+    var damage = randomNumber(enemyAttack - 3, enemyAttack);
+
+    playerHealth = Math.max(0, playerHealth - damage);
+
     console.log(
       enemyName + ' attacked ' + playerName + '. ' + playerName + ' now has ' + playerHealth + ' health remaining.'
     );
@@ -85,8 +90,7 @@ var fight = function(enemyName) {
     var pickedEnemyName = enemyNames[i];
 
     // reset enemyHealth before starting new fight
-    enemyHealth = 50;
-
+    enemyHealth = randomNumber(40, 60);
     // use debugger to pause script from running and check what's going on at that moment in the code
     // debugger;
 
@@ -191,3 +195,9 @@ var endGame = function(){
     window.alert("Thank you for playing Robot Gladiators! Come back soon!")
   }
 }
+// function to generate a random numeric value
+var randomNumber = function(min, max) {
+  var value = Math.floor(Math.random() * (max - min + 1) + min)
+;
+  return value;
+};
