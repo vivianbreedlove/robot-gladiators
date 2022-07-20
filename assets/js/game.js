553e6d668a3f0d56077281edee4055459bb6137c
vivianbreedlove <vivianbreedlove@github.com>
2022-07-19 19:12:56 -0500

    Fix Prompt Bugs in the fight() Function

--- assets/js/game.js
+++ assets/js/game.js
@@ -4,12 +4,48 @@ var randomNumber = function(min, max){
   return value;
 };
 
+var fightOrSkip = function(){
+  //ask player if they'd like to fightOrSkip function
+  var promptFight = window.prompt('Would you like to FIGHT or SKIP this battle? Enter "FIGHT" or "SKIP" to choose. ')
+  promptFight = promptFight.toLowerCase();
+
+  //Conditional Recursive Function Call
+  if (promptFight === "" || promptFight === null){
+    window.alert("You need to provide a valid answer! Please try again. ");
+    return fightOrSkip();
+  }
+
+
+  //if player picks "skip" confirm and then stop the loop
+  if (promptFight === "skip" || promptFight === "SKIP"){
+    //confirm player wants to skip
+    var confirmSkip = window.confirm("Are you sure you'd like to quit?");
+
+    //if yes (true), leave fight
+    if (confirmSkip){
+      window.alert(playerInfo.name + " has decided to skip this fight. Goodbye! ")
+      //subtract money from playerMoney for skipping, but don't let them go into the negative
+     playerInfo.money =Math.max(0, playerInfo.money - 10); 
+     //return true if player wants to leave
+     return true;
+     }
+
+     shop();
+    }
+  return false}
 
 // fight function (now with parameter for enemy's name)
-var fight = function(enemy) {
+var fight = function(enemy)
+//repeat and execute as long as the enemy-robot is alive
+ {
   while (playerInfo.health > 0 && enemy.health > 0) {
-    // ask player if they'd like to fight or run
-    var promptFight = window.prompt('Would you like to FIGHT or SKIP this battle? Enter "FIGHT" or "SKIP" to choose.');
+    //ask player if thye'd like to fight or skip using fightOrSkip function
+   if (fightOrSkip()){
+     //if true, leave fight by breaking loop
+     break;
+     
+   };
+    var damage = randomNumber(playerInfo.attack - 3 , playerInfo.attack );
 
     // if player picks "skip" confirm and then stop the loop
     if (promptFight === "skip" || promptFight === "SKIP") {
